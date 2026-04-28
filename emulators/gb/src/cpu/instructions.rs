aea3c850f44947a10007542480a4e02e645de9bc
Nicolas Zordan <nicolas.zordan.tek@gmail.com>
2026-04-28 16:51:17 +0200

    style(cpu): update comments for lint suppression in operand and parameter modules

--- emulators/gb/src/cpu/instructions.rs
+++ emulators/gb/src/cpu/instructions.rs
@@ -7,8 +7,10 @@ pub mod execute;
 #[allow(unused)] // while we wait for the debug interface
 pub mod meta;
 
-#[allow(clippy::upper_case_acronyms)] // we're suppressing this lint to keep the naming consistent with the pan docs
+// we're suppressing this lint to keep the naming consistent with the pan docs
+#[allow(clippy::upper_case_acronyms)]
 pub mod operand;
 
-#[allow(clippy::upper_case_acronyms)] // we're suppressing this lint to keep the naming consistent with the pan docs
+// we're suppressing this lint to keep the naming consistent with the pan docs
+#[allow(clippy::upper_case_acronyms)]
 pub mod parameter;
